9a2409a2e3562c7e70f6ab297a9a50785aca51fc
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-03-08 11:30:56 +0800

    put login in pages instead

--- chatbot-app/src/App.tsx
+++ chatbot-app/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
+import Login from "./pages/Login";
 import TempPage from "./pages/TempPage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import { AuthProvider } from "./auth/useAuth";
