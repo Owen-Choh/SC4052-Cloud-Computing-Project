31f1439e88dd2a608ebe16df8174da342b920819
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-03-09 21:02:29 +0800

    test jwt and password funcs

---
+++ chatbot-backend/chatbot/auth/password_test.go
@@ -0,0 +1,41 @@
+package auth
+
+import (
+	"testing"
+)
+
+func TestHashPassword(t *testing.T) {
+	plaintext := "password"
+	
+	hash, err := HashPassword(plaintext)
+	if err != nil {
+		t.Errorf("error hashing password: %v", err)
+	}
+
+	if hash == "" {
+		t.Error("expected hash to not be empty")
+	}
+
+	if hash == plaintext {
+		t.Error("expected hash to be different from plaintext")
+	}
+}
+
+func TestComparePassword(t *testing.T) {
+	plaintext := "password"
+
+	hash, err := HashPassword(plaintext)
+	if err != nil {
+		t.Errorf("error hashing password: %v", err)
+	}
+
+	if !ComparePassword(hash, []byte(plaintext)) {
+		t.Errorf("error password to match hash")
+	}
+
+	if ComparePassword(hash, []byte(plaintext)){
+		t.Errorf("error password to not match hash")
+	}
+
+}
+
