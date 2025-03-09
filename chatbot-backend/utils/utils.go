028ffabaab680d057f2aa5956b3e3a6c3d5bc40f
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-03-09 16:11:04 +0800

    add helper function to write json and errors

--- chatbot-backend/utils/utils.go
+++ chatbot-backend/utils/utils.go
@@ -7,9 +7,20 @@ import (
 )
 
 func ParseJson(r *http.Request, payload any) error {
-	if r.Body == nil{
+	if r.Body == nil {
 		return fmt.Errorf("missing request body")
 	}
 
 	return json.NewDecoder(r.Body).Decode(payload)
+}
+
+func WriteJson(w http.ResponseWriter, statusCode int, payload any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	return json.NewEncoder(w).Encode(payload)
+}
+
+func WriteError(w http.ResponseWriter, statusCode int, err error) {
+	WriteJson(w, statusCode, map[string]string{"error": err.Error()})
 }
