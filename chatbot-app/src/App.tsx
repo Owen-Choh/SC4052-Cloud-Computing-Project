6dbda379fe8907c34f431d27827c163ea16a30de
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-03-30 17:36:24 +0800

    remove extra pages

--- chatbot-app/src/App.tsx
+++ chatbot-app/src/App.tsx
@@ -5,24 +5,17 @@ import {
   Navigate,
 } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-import TempPage from "./pages/TempPage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import { AuthProvider } from "./auth/useAuth";
 import Dashboard from "./pages/Dashboard";
 import ConversationPage from "./pages/ConversationPage";
 import { ChatbotProvider } from "./context/ChatbotContext";
-import GeminiStreamDisplay from "./pages/GeminiStreamDisplay";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <Routes>
-          <Route
-            path="/test/chat/:username/:chatbotname"
-            element={<GeminiStreamDisplay />}
-          />
-
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<LoginPage />} />
           <Route
@@ -30,7 +23,6 @@ function App() {
             element={<ConversationPage />}
           />
 
-          <Route path="/TempPage" element={<TempPage />} />
           <Route element={<ProtectedRoute />}>
             <Route
               path="/Dashboard"
