f4d4fe0f252907c0ff4a336f700a4c0230b5e89b
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-03-30 16:28:03 +0800

    added tests for register handler

--- chatbot-backend/chatbot/types/types.go
+++ chatbot-backend/chatbot/types/types.go
@@ -62,8 +62,8 @@ type RegisterUserPayload struct {
 }
 
 type LoginUserPayload struct {
-	Username string `json:"username" validate:"required,min=3,alphanum"`
-	Password string `json:"password" validate:"required,min=8"`
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required,min=3"`
 }
 
 // ChatRequest defines the request body for chatting with a chatbot.
