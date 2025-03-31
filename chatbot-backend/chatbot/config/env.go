fbbdbae4bd6981310d93e7b866eca07ebd255880
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-03-31 11:40:34 +0800

    change GEMINI_API_KEY to os env variable

--- chatbot-backend/chatbot/config/env.go
+++ chatbot-backend/chatbot/config/env.go
@@ -33,15 +33,25 @@ func initConfig() Config {
 		Port:                     getEnv("BACKEND_PORT", "8080"),
 		DATABASE_PATH:            getEnv("DATABASE_PATH", "./database_files/chatbot.db"),
 		FILES_PATH:               getEnv("FILES_PATH", "database_files/uploads/"),
-		Default_Time:             getEnv("Default_Time","20 Mar 25 15:32 +0800"),
-		Time_layout:              getEnv("Time_layout","02 Jan 06 15:04 -0700"),
-		Timezone:                 getEnv("Timezone","Asia/Singapore"),
+		Default_Time:             getEnv("Default_Time", "20 Mar 25 15:32 +0800"),
+		Time_layout:              getEnv("Time_layout", "02 Jan 06 15:04 -0700"),
+		Timezone:                 getEnv("Timezone", "Asia/Singapore"),
 		JWTExpirationInSeconds:   getEnvInt("JWT_EXP_SECONDS", 3600*24*1),
 		JWTSecret:                getEnv("JWT_SECRET", "should-have-jwt-secret-here"),
 		API_FILE_EXPIRATION_HOUR: getEnvInt("API_FILE_EXPIRATION_HOUR", 47),
-		GEMINI_API_KEY:           getEnv("GEMINI_API_KEY", ""),
 		MODEL_NAME:               getEnv("MODEL_NAME", "gemini-2.0-flash-thinking-exp-01-21"),
+		GEMINI_API_KEY:           getOSEnv("GEMINI_API_KEY", ""),
 	}
+}
+
+func getOSEnv(key string, fallback string) string {
+	value := os.Getenv("key") // Get API key from environment variable
+	if value == "" {
+		log.Printf("OS Environment variable %s not set, using fallback value: %s", key, fallback)
+		return fallback
+	}
+
+	return value
 }
 
 func getEnv(key string, fallback string) string {
